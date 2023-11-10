ad761cee618660eb52019790d6b106c23f04eac2
Acchindra Thev <acchindrathev@wirelessprv-10-193-210-150.near.illinois.edu>
2023-11-10 13:00:15 -0600

    added notification infrastructure

--- app/src/main/java/edu/illinois/cs465/myquizappwithlifecycle/MainActivity.java
+++ app/src/main/java/edu/illinois/cs465/myquizappwithlifecycle/MainActivity.java
@@ -1,11 +1,20 @@
 package edu.illinois.cs465.myquizappwithlifecycle;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -24,6 +33,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             String value = new String(savedInstanceState.getString(KEY));
             Log.d(DEBUG, value);
         }
+        createNotificationChannel();
+        showNotification();
 
 //        falseButton = (Button) findViewById(R.id.false_button);
 //        trueButton = (Button) findViewById(R.id.true_button);
@@ -42,22 +53,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onStart();
         Log.d(DEBUG, "OnStart()");
     }
+
     protected void onResume() {
         super.onResume();
         Log.d(DEBUG, "OnResume()");
     }
+
     protected void onRestart() {
         super.onRestart();
         Log.d(DEBUG, "OnRestart()");
     }
+
     protected void onPause() {
         super.onPause();
         Log.d(DEBUG, "OnPause()");
     }
+
     protected void onStop() {
         super.onStop();
         Log.d(DEBUG, "OnStop()");
     }
+
     protected void onDestroy() {
         super.onDestroy();
         Log.d(DEBUG, "OnDestroy()");
@@ -71,6 +87,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Toast.makeText(this, "INCORRECT", Toast.LENGTH_SHORT).show();
         }
     }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Free Food Alert!";
+            String description = "Pizza Available at Siebel CS NOW!";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("channel_id", name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private void showNotification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "channel_id")
+                .setSmallIcon(R.drawable.account_circle_24px) // replace with your own icon
+                .setContentTitle("Free Food Alert!")
+                .setContentText("Pizza Available at Siebel CS NOW!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        notificationManager.notify(1, builder.build());
+    }
+
 //    bottomAppBar.setNavigationOnClickListener {
 //            // Handle navigation icon press
 //    }
