bbf3aecd9b22dfceac70f6f2ff2f740ce522eb5c
Bryan Finn <bryan.finn8@gmail.com>
2023-11-17 23:41:24 -0600

    fixed main to go to maps w/o crashing :)

--- app/src/main/java/edu/illinois/cs465/myquizappwithlifecycle/MainActivity.java
+++ app/src/main/java/edu/illinois/cs465/myquizappwithlifecycle/MainActivity.java
@@ -67,87 +67,87 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 //        RsoBaseScreenBinding binding = RsoBaseScreenBinding.inflate(getLayoutInflater());
 //        setContentView(binding.getRoot());
-
-        if (savedInstanceState != null) {
-            String value = new String(savedInstanceState.getString(KEY));
-            if (value != null) {
-                Log.d(DEBUG, value);
-            }
-        }
-
-        findViewById(R.id.deleted_post_button).setOnClickListener(v -> {
-            showDeletedEventPopup();
-        });
-
-        findViewById(R.id.food_popup).setOnClickListener(v -> {
-            // Inflater to be able to grab button in dialog
-            LayoutInflater inflater = getLayoutInflater();
-            View dialogLayout = inflater.inflate(R.layout.food_popup, null);
-//        RsoBaseScreenBinding binding = RsoBaseScreenBinding.inflate(getLayoutInflater());
-//        binding.floatingActionButton.setOnClickListener(view -> showBottomDialog());
-
-//        findViewById(R.id.floating_action_button).setOnClickListener(v -> {
-//            showBottomDialog();
-        });
-
-//////////////////////// TO MAPS
-//        ImageView accountCircleImage = findViewById(R.id.account_circle_image);
 //
-//        accountCircleImage.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                // Intent to navigate to MainActivity
-//                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
-//                startActivity(intent);
+//        if (savedInstanceState != null) {
+//            String value = new String(savedInstanceState.getString(KEY));
+//            if (value != null) {
+//                Log.d(DEBUG, value);
 //            }
-//        });
-/////////////////////
+//        }
+//
+//        findViewById(R.id.deleted_post_button).setOnClickListener(v -> {
+//            showDeletedEventPopup();
 //        });
 //
 //        findViewById(R.id.food_popup).setOnClickListener(v -> {
 //            // Inflater to be able to grab button in dialog
 //            LayoutInflater inflater = getLayoutInflater();
 //            View dialogLayout = inflater.inflate(R.layout.food_popup, null);
-//
 ////        RsoBaseScreenBinding binding = RsoBaseScreenBinding.inflate(getLayoutInflater());
-////        binding.floatingActionButton.setOnClickListener(v -> showBottomDialog());
+////        binding.floatingActionButton.setOnClickListener(view -> showBottomDialog());
 //
 ////        findViewById(R.id.floating_action_button).setOnClickListener(v -> {
 ////            showBottomDialog();
+//        });
+//
+////////////////////////// TO MAPS
+////        ImageView accountCircleImage = findViewById(R.id.account_circle_image);
+////
+////        accountCircleImage.setOnClickListener(new View.OnClickListener() {
+////            @Override
+////            public void onClick(View v) {
+////                // Intent to navigate to MainActivity
+////                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+////                startActivity(intent);
+////            }
 ////        });
-////        falseButton = (Button) findViewById(R.id.false_button);
-////        trueButton = (Button) findViewById(R.id.true_button);
+///////////////////////
+////        });
+////
+////        findViewById(R.id.food_popup).setOnClickListener(v -> {
+////            // Inflater to be able to grab button in dialog
+////            LayoutInflater inflater = getLayoutInflater();
+////            View dialogLayout = inflater.inflate(R.layout.food_popup, null);
+////
+//////        RsoBaseScreenBinding binding = RsoBaseScreenBinding.inflate(getLayoutInflater());
+//////        binding.floatingActionButton.setOnClickListener(v -> showBottomDialog());
+////
+//////        findViewById(R.id.floating_action_button).setOnClickListener(v -> {
+//////            showBottomDialog();
+//////        });
+//////        falseButton = (Button) findViewById(R.id.false_button);
+//////        trueButton = (Button) findViewById(R.id.true_button);
+////
+//////        falseButton.setOnClickListener(this);
+//////        trueButton.setOnClickListener(this);
+////            showFoodInfoPopup(dialogLayout);
+////
+////            Button seeMoreButton = (Button)dialogLayout.findViewById(R.id.see_more_button);
+////            seeMoreButton.setOnClickListener(b -> {
+////                Intent intent = new Intent(this, FoodInfoActivity.class);
+////                startActivity(intent);
+////            });
+////
+////        });
+////
+////        findViewById(R.id.post_food).setOnClickListener(v -> {
+////            Intent intent = new Intent(this, FoodPostActivity.class);
+////            startActivity(intent);
+////        });
+////
+////        findViewById(R.id.RSO_View).setOnClickListener(v -> {
+////            Intent intent = new Intent(this, RSOActivity.class);
+////            startActivity(intent);
+////        });
 //
-////        falseButton.setOnClickListener(this);
-////        trueButton.setOnClickListener(this);
-//            showFoodInfoPopup(dialogLayout);
-//
-//            Button seeMoreButton = (Button)dialogLayout.findViewById(R.id.see_more_button);
-//            seeMoreButton.setOnClickListener(b -> {
-//                Intent intent = new Intent(this, FoodInfoActivity.class);
-//                startActivity(intent);
-//            });
-//
-//        });
-//
-//        findViewById(R.id.post_food).setOnClickListener(v -> {
-//            Intent intent = new Intent(this, FoodPostActivity.class);
-//            startActivity(intent);
-//        });
-//
-//        findViewById(R.id.RSO_View).setOnClickListener(v -> {
-//            Intent intent = new Intent(this, RSOActivity.class);
-//            startActivity(intent);
-//        });
-
-        ImageView accountCircleImage = findViewById(R.id.account_circle_image);
-        accountCircleImage.setOnClickListener(v -> showBottomDialog());
-//        accountCircleImage.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                showBottomDialog
-//            }
-//        });
+//        ImageView accountCircleImage = findViewById(R.id.account_circle_image);
+//        accountCircleImage.setOnClickListener(v -> showBottomDialog());
+////        accountCircleImage.setOnClickListener(new View.OnClickListener() {
+////            @Override
+////            public void onClick(View v) {
+////                showBottomDialog
+////            }
+////        });
     }
 
     protected void onSaveInstanceState(Bundle savedInstance) {
@@ -194,109 +194,109 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Toast.makeText(this, "INCORRECT", Toast.LENGTH_SHORT).show();
         }
     }
-
-    private void createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = "Free Food Alert!";
-            String description = "Pizza Available at Siebel CS NOW!";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel("channel_id", name, importance);
-            channel.setDescription(description);
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
-    }
-
-    private void showNotification() {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "channel_id")
-                .setSmallIcon(R.drawable.account_circle_24px) // replace with your own icon
-                .setContentTitle("Free Food Alert!")
-                .setContentText("Pizza Available at Siebel CS NOW!")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        notificationManager.notify(1, builder.build());
-    }
-
-//    bottomAppBar.setNavigationOnClickListener {
-//            // Handle navigation icon press
+//
+//    private void createNotificationChannel() {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            CharSequence name = "Free Food Alert!";
+//            String description = "Pizza Available at Siebel CS NOW!";
+//            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+//            NotificationChannel channel = new NotificationChannel("channel_id", name, importance);
+//            channel.setDescription(description);
+//            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+//            notificationManager.createNotificationChannel(channel);
+//        }
 //    }
 //
-//    bottomAppBar.setOnMenuItemClickListener { menuItem ->
-//            when (menuItem.itemId) {
-//                R.id.search -> {
-//                    // Handle search icon press
-//                    true
-//                }
-//                R.id.more -> {
-//                    // Handle more item (inside overflow menu) press
-//                    true
-//                }
-//                else -> false
-//            }
+//    private void showNotification() {
+//        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "channel_id")
+//                .setSmallIcon(R.drawable.account_circle_24px) // replace with your own icon
+//                .setContentTitle("Free Food Alert!")
+//                .setContentText("Pizza Available at Siebel CS NOW!")
+//                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+//
+//        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+//        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
 //        }
-
-    private void showDeletedEventPopup() {
-        AlertDialog dialog = new MaterialAlertDialogBuilder(MainActivity.this)
-                .setMessage("Post has been deleted by host")
-                .setPositiveButton("Ok", null)
-                .show();
-    }
-
-    private void showFoodInfoPopup(View dialog_layout) {
-        Dialog dialog = new Dialog(MainActivity.this);
-        dialog.setContentView(dialog_layout);
-        dialog.show();
-    }
-    private void showBottomDialog() {
-        final Dialog dialog = new Dialog(this);
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        dialog.setContentView(R.layout.bottomsheet_layout);
-
-        LinearLayout rso1 = dialog.findViewById(R.id.rso1);
-        LinearLayout student1 = dialog.findViewById(R.id.student1);
-        ImageView cancelButton = dialog.findViewById(R.id.cancelButton);
-
-        rso1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.dismiss();
-                Toast.makeText(MainActivity.this, "Switching to RSO 1 screen", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(MainActivity.this, RSOActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        student1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.dismiss();
-                Toast.makeText(MainActivity.this, "Switching to Student 1 screen", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
-                startActivity(intent);
-            }
-        });
-        cancelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.dismiss();
-            }
-        });
-
-        dialog.show();
-        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        dialog.getWindow().getAttributes().windowAnimations = com.google.android.material.R.style.MaterialAlertDialog_Material3_Animation;
-        dialog.getWindow().setGravity(Gravity.BOTTOM);
-    }
+//        notificationManager.notify(1, builder.build());
+//    }
+//
+////    bottomAppBar.setNavigationOnClickListener {
+////            // Handle navigation icon press
+////    }
+////
+////    bottomAppBar.setOnMenuItemClickListener { menuItem ->
+////            when (menuItem.itemId) {
+////                R.id.search -> {
+////                    // Handle search icon press
+////                    true
+////                }
+////                R.id.more -> {
+////                    // Handle more item (inside overflow menu) press
+////                    true
+////                }
+////                else -> false
+////            }
+////        }
+//
+//    private void showDeletedEventPopup() {
+//        AlertDialog dialog = new MaterialAlertDialogBuilder(MainActivity.this)
+//                .setMessage("Post has been deleted by host")
+//                .setPositiveButton("Ok", null)
+//                .show();
+//    }
+//
+//    private void showFoodInfoPopup(View dialog_layout) {
+//        Dialog dialog = new Dialog(MainActivity.this);
+//        dialog.setContentView(dialog_layout);
+//        dialog.show();
+//    }
+//    private void showBottomDialog() {
+//        final Dialog dialog = new Dialog(this);
+//        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        dialog.setContentView(R.layout.bottomsheet_layout);
+//
+//        LinearLayout rso1 = dialog.findViewById(R.id.rso1);
+//        LinearLayout student1 = dialog.findViewById(R.id.student1);
+//        ImageView cancelButton = dialog.findViewById(R.id.cancelButton);
+//
+//        rso1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                dialog.dismiss();
+//                Toast.makeText(MainActivity.this, "Switching to RSO 1 screen", Toast.LENGTH_SHORT).show();
+//                Intent intent = new Intent(MainActivity.this, RSOActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        student1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                dialog.dismiss();
+//                Toast.makeText(MainActivity.this, "Switching to Student 1 screen", Toast.LENGTH_SHORT).show();
+//                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//        cancelButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                dialog.dismiss();
+//            }
+//        });
+//
+//        dialog.show();
+//        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+//        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+//        dialog.getWindow().getAttributes().windowAnimations = com.google.android.material.R.style.MaterialAlertDialog_Material3_Animation;
+//        dialog.getWindow().setGravity(Gravity.BOTTOM);
+//    }
 }
