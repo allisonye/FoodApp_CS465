3f0c38572fdbcd9a4d508dd12ab81b2d27e10f4a
Brenda Hariyanto <brendah5@illinois.edu>
2023-11-29 13:43:42 -0600

    updation works

--- app/src/main/java/edu/illinois/cs465/myquizappwithlifecycle/data/FoodListingDao.java
+++ app/src/main/java/edu/illinois/cs465/myquizappwithlifecycle/data/FoodListingDao.java
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import androidx.room.Update;
 
@@ -12,7 +13,7 @@ import java.util.List;
 
 @Dao
 public abstract class FoodListingDao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     abstract void insert(FoodListing foodListing);
 
     void insertWithTimestamp(FoodListing foodListing) {
