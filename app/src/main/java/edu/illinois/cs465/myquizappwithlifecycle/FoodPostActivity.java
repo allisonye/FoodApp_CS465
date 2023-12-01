2f661325a795112ee0b0066cdb7b600a76a72c16
Allison <ay14@illinois.edu>
2023-12-01 09:41:11 -0600

    fix restriction filters on map

--- app/src/main/java/edu/illinois/cs465/myquizappwithlifecycle/FoodPostActivity.java
+++ app/src/main/java/edu/illinois/cs465/myquizappwithlifecycle/FoodPostActivity.java
@@ -234,10 +234,12 @@ private String apiKey;
     private int getDietaryRestrictionChipId(String restriction) {
         // Map restriction strings to chip IDs
         switch (restriction) {
-            case "Gluten-Free": return R.id.chip1;
+            case "Gluten-free": return R.id.chip1;
             case "Dairy-free": return R.id.chip2;
             case "Vegetarian": return R.id.chip3;
-            // Add other mappings here
+            case "Nut-free": return R.id.chip4;
+            case "Shellfish-free": return R.id.chip5;
+            case "Vegan": return R.id.chip6;
             default: return -1;
         }
     }
